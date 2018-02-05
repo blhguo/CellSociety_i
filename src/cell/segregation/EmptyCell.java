3b44d748478892e81a44ebec2c24c948c1b3cb7b
Yashas Manjunatha <ym101@duke.edu>
2018-02-05 17:00:05 -0500

    Updated Segregation simulation

--- src/cell/segregation/EmptyCell.java
+++ src/cell/segregation/EmptyCell.java
@@ -6,14 +6,24 @@ import cell.Cell;
 import javafx.scene.paint.Color;
 
 public class EmptyCell extends SegregationSimCell{
+	private boolean isTaken = false;
 
 	public EmptyCell() {
-		this.DISPLAYCOLOR = Color.GREY;
+		isTaken = false;
+		this.DISPLAYCOLOR = Color.LIGHTGRAY;
 	}
 	
 	@Override
 	public Cell nextState(HashSet<Cell> neighbors) {
 		return this;
+	}
+
+	public boolean isTaken() {
+		return isTaken;
+	}
+
+	public void setTaken(boolean isTaken) {
+		this.isTaken = isTaken;
 	}
 
 }
--- src/grid/SegregationSimGrid.java
+++ src/grid/SegregationSimGrid.java
@@ -28,18 +28,59 @@ public class SegregationSimGrid extends Grid{
 		}
 	}
 	
+	
+	private Cell[][] nextGrid;
 	@Override
 	public Cell[][] updateGrid() {
-		Cell[][] nextGrid = new Cell[this.myGrid.length][this.myGrid[0].length];
+		nextGrid = new Cell[this.myGrid.length][this.myGrid[0].length];
 		for (int i = 0; i < nextGrid.length; i++) {
 			for (int j = 0; j < nextGrid[0].length; j++) {
 				HashSet<Cell> neighbors = new HashSet<Cell>();
 				this.addNeighbors(neighbors, myGrid, i , j);
-				nextGrid[i][j] = myGrid[i][j].nextState(neighbors);
+				Cell nextState = myGrid[i][j].nextState(neighbors);
+				if (!(nextState instanceof EmptyCell))
+					nextGrid[i][j] = nextState;
+				else if (!(myGrid[i][j] instanceof EmptyCell)) {
+					placeInEmpty(myGrid[i][j]);
+					nextGrid[i][j] = nextState;
+				} else if (!((EmptyCell) myGrid[i][j]).isTaken())
+					nextGrid[i][j] = nextState;
 			}
 		}
 		myGrid = nextGrid;
 		return nextGrid;
+	}
+	
+	private void placeInEmpty (Cell cell) {
+		for (int i = 0; i < myGrid.length; i++) {
+			for (int j = 0; j < myGrid[0].length; j++) {
+				if (myGrid[i][j] instanceof EmptyCell && !((EmptyCell) myGrid[i][j]).isTaken()) {
+					nextGrid[i][j] = cell;
+					((EmptyCell) myGrid[i][j]).setTaken(true);
+					return;
+				}
+			}
+		}		
+	}
+	
+	@Override
+	protected void addNeighbors(HashSet<Cell> neighbors, Cell[][] grid, int i, int j) {
+		if (inGrid(i-1,j))
+			neighbors.add(grid[i-1][j]);
+		if (inGrid(i+1,j))
+			neighbors.add(grid[i+1][j]);
+		if (inGrid(i,j-1))
+			neighbors.add(grid[i][j-1]);
+		if (inGrid(i,j+1))
+			neighbors.add(grid[i][j+1]);
+		if (inGrid(i-1,j-1))
+			neighbors.add(grid[i-1][j-1]);
+		if (inGrid(i+1,j+1))
+			neighbors.add(grid[i+1][j+1]);
+		if (inGrid(i+1,j-1))
+			neighbors.add(grid[i+1][j-1]);
+		if (inGrid(i-1,j+1))
+			neighbors.add(grid[i-1][j+1]);
 	}
 
 }
