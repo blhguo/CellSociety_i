0734ef44023d33215dcf636da1593f74208ac957
Yashas Manjunatha <ym101@duke.edu>
2018-02-11 20:45:46 -0500

    Included Files for Sugar Simulation

---
+++ src/cell/sugarsim/SugarSimCell.java
@@ -0,0 +1,34 @@
+package cell.sugarsim;
+
+import cell.Cell;
+
+public abstract class SugarSimCell extends Cell{
+	protected int patch_sugar;
+	protected int max_sugar;
+	protected int sugarGrowBackRate;
+	protected int sugarGrowBackInterval;
+	protected int tick;
+	
+	public SugarSimCell(int patch_sugar, int max_sugar, int sugarGBR, int sugarGBI, int tick) {
+		this.patch_sugar = patch_sugar;
+		this.max_sugar = max_sugar;
+		this.sugarGrowBackRate = sugarGBR;
+		this.sugarGrowBackInterval = sugarGBI;
+		this.tick = tick;
+	}
+	
+	protected void patchGrowBack() {
+		if (tick%sugarGrowBackInterval == 0) {
+			patch_sugar += sugarGrowBackRate;
+			if (patch_sugar > max_sugar) {
+				patch_sugar = max_sugar;
+			}
+		}
+	}
+	
+	@Override
+	protected void setDisplayColor() {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/grid/SugarSimGrid.java
@@ -0,0 +1,35 @@
+package grid;
+
+import java.util.Map;
+
+public class SugarSimGrid extends Grid{
+
+	public SugarSimGrid(int width, int height, String shape, String arrangement, String edge_type) {
+		super(width, height, shape, arrangement, edge_type);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Map<String, Number> getNumberOfCells() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[][] getArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Double[]> getCurrentParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCurrentParameters(Map<String, Double[]> map) {
+		// TODO Auto-generated method stub
+		
+	}
+}
